95c10b45c1e7e13f9153682de83cfcc7e2a4c2e8
Mustaqeem <Mustaqeem.A.Chowdhury@gmail.com>
2026-03-07 15:41:32 +0000

    basic responsiveness

--- pages/Vans/VanDetail.jsx
+++ pages/Vans/VanDetail.jsx
@@ -36,25 +36,30 @@ export default function VanDetail() {
     const type = location.state?.type || "all";
     
     return (
-        <div className="van-detail-container">
-            <Link
+        <>
+                    <Link
                 to={`..${search}`}
                 relative="path"
                 className="back-button"
             >&larr; <span>Back to {type} vans</span></Link>
             
-            {van && (
-                <div className="van-detail">
-                    <img src={van.imageUrl} />
-                    <i className={`van-type ${van.type} selected`}>
-                        {van.type}
-                    </i>
-                    <h2>{van.name}</h2>
-                    <p className="van-price"><span>${van.price}</span>/day</p>
-                    <p>{van.description}</p>
-                    <button className="link-button">Rent this van</button>
-                </div>
-            )}
-        </div>
+            <div className="van-detail-container">
+
+                {van && (
+                    <div className="van-detail">
+                        <img src={van.imageUrl} />
+                        <i className={`van-type ${van.type} selected`}>
+                            {van.type}
+                        </i>
+                        <h2>{van.name}</h2>
+                        <p className="van-price"><span>${van.price}</span>/day</p>
+                        <p>{van.description}</p>
+                        <button className="link-button">Rent this van</button>
+                    </div>
+                )}
+            </div>
+        
+        </>
+        
     )
 }
